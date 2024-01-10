af7e6808d67f3e566795abc184db890e0fe0da74
AntonioMaggi <maggia1013@gmail.com>
2024-01-10 16:34:07 +0100

    added navigation to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule, MatListModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  sidenav: any;
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
+  ngOnInit() {
+    this.breakpointObserver.observe([
+      Breakpoints.HandsetPortrait,
+      Breakpoints.HandsetLandscape,
+    ]).subscribe(result => {
+      if (result.matches) {
+        // Adjust sidebar for mobile view
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        // Adjust sidebar for desktop view
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
 }
